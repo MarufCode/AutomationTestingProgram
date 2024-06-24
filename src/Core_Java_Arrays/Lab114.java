4f608161e48393eece42d5f766ee34540e10cab0
Maruf <marufsherikar32@gmail.com>
2024-06-24 11:12:09 +0530

    24

---
+++ src/Core_Java_Arrays/Lab114.java
@@ -0,0 +1,26 @@
+package src.Core_Java_Arrays;
+
+
+public class Lab114 {
+    public static void main(String[] args) {
+
+
+        // Create a Function
+        // Argument or Parameter
+        // Name of the function
+        // Access Type
+        // Return Type
+
+       // sum -> 2 , return int, sum, public
+
+        float d = sum(3.3f, 4);
+        System.out.println(d);
+
+    }
+
+    public static float sum(float maruf, float sherikar){
+        return maruf + sherikar;
+
+    }
+
+}
