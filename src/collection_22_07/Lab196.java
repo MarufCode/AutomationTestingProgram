b6fc02e0b13ec422676991b615a92a33a313c951
Maruf <marufsherikar32@gmail.com>
2024-07-26 16:16:02 +0530

    Collection framework hashmap and hashtable

---
+++ src/collection_22_07/Lab196.java
@@ -0,0 +1,25 @@
+package src.collection_22_07;
+
+import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Lab196 {
+    public static void main(String[] args) {
+
+
+        Queue integerQueue = new PriorityQueue<>();
+        integerQueue.add("A");
+        integerQueue.add("B");
+        integerQueue.add("C");
+        System.out.println(integerQueue);
+        integerQueue.poll();
+        System.out.println(integerQueue);
+
+        Iterator iterator = integerQueue.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+    }
+}
