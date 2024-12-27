9fb2abe930c9ac6b40d890c160fd21b9611cca02
Maruf <marufsherikar32@gmail.com>
2024-12-27 11:12:10 +0530

    changes

--- src/JavaPrograms/LargestNumberinArray.java
+++ src/JavaPrograms/LargestNumberinArray.java
@@ -2,16 +2,28 @@ package src.JavaPrograms;
 
 public class LargestNumberinArray {
     public static void main(String[] args) {
+//
+//
+//                int[] arr = {10, 20, 30, 40, 50};
+//                int largest = arr[0];
+//
+//                for (int num : arr) {
+//                    if (num > largest) largest = num;
+//                }
+//
+//                System.out.println("Largest number: " + largest);
 
-
-                int[] arr = {10, 20, 30, 40, 50};
-                int largest = arr[0];
-
-                for (int num : arr) {
-                    if (num > largest) largest = num;
+                try {
+                    int result = 10 / 0;  // This will throw ArithmeticException
+                    System.out.println("Result: " + result);
+                } catch (ArithmeticException e) {
+                    System.out.println("Exception caught: " + e.getMessage());
+                } finally {
+                    System.out.println("Inside finally block");
                 }
+                System.out.println("Program continues...");
 
-                System.out.println("Largest number: " + largest);
+
 
 
     }
--- src/JavaPrograms/VowelCount.java
+++ src/JavaPrograms/VowelCount.java
@@ -4,11 +4,14 @@ public class VowelCount {
 
     public static void main(String[] args) {
 
-        String x = "Maruf Sherikar";
+        String x = "MAruf ShErikar";
         int count = 0;
 
         for (int i = 0; i<x.length(); i++){
             Character c = x.charAt(i);
+
+            c =  Character.toLowerCase(c);
+
             if (c == 'a' || c == 'e'|| c == 'i'|| c == 'o'|| c == 'u'){
                 count = count + 1;
 
