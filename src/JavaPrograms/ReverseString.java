8794a1958c093802976fde8c665d903d6fcee8ae
Maruf <marufsherikar32@gmail.com>
2025-04-11 16:14:41 +0530

    Programs

--- src/JavaPrograms/ReverseString.java
+++ src/JavaPrograms/ReverseString.java
@@ -3,15 +3,28 @@ package src.JavaPrograms;
 public class ReverseString {
     public static void main(String[] args) {
 
+//        String x = "Maruf";
+//        String y = "";
+//
+//
+//        for (int i = x.length()-1;i>=0; i--){
+//            y = y + x.charAt(i);
+//
+//        }
+//
+//        System.out.println(y);
+
         String x = "Maruf";
+
         String y = "";
 
+        for (int i =x.length()-1; i>=0; i--){
 
-        for (int i = x.length()-1;i>=0; i--){
             y = y + x.charAt(i);
 
         }
 
         System.out.println(y);
+
     }
 }
